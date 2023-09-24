b4275f29c286470efbb00890961be78777a2c65a
Kaszás <kaszas.d.mark@gmail.com>
2023-09-24 23:41:26 +0200

    javitando

--- mosze/mosze.cpp
+++ mosze/mosze.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int* b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-        for (int i = 0;)
+    int* b = new int[NELEMENTS]; // kimaradt egy _ jel
+    std::cout << '1-100 ertekek duplazasa' // idézõjel rossz
+        for (int i = 0;) //for ciklus hiányos
         {
             b[i] = i * 2;
         }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // nincs kikötés
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // nincs kiírt érték
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // átlagnak hiányzik az értékadás
+    for (int i = 0; i < N_ELEMENTS, i++) // for ciklusban , helyett ; jel kellene
     {
-        atlag += b[i]
+        atlag += b[i] // elmaradt egy ; jel
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
